69514caf387ef95586db98d36e2835539927a052
shivam barsaley <shivam_barsaley@mckinsey.com>
2017-04-12 16:55:49 +0530

    Updating the config to store the seeders progress in DB

--- config/config.js
+++ config/config.js
@@ -4,20 +4,24 @@ module.exports = {
     "password": null,
     "database": "todo_dev",
     "host": "localhost",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "seederStorage": "sequelize"
   },
   "test": {
     "username": "root",
     "password": null,
     "database": "todo_test",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "seederStorage": "sequelize"
+
   },
   "production": {
     "username": "root",
     "password": null,
     "database": "todo_prod",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "seederStorage": "sequelize"
   }
 }
