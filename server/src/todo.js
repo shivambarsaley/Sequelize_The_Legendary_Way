bdaa598ac6d135d355900452ace83baf02fddf5f
shivam barsaley <shivam_barsaley@mckinsey.com>
2017-05-03 21:19:38 +0530

    Added basic operations for Todo

--- server/src/todo.js
+++ server/src/todo.js
@@ -1,12 +1,43 @@
+'use strict';
 const Model = require('../models/index');
 
-const getTodosForUser = (userId, includeTasks = false) => {
+const getTodosForUser = (UserId, includeTasks = true) => {
   const options = {
-    where: {userId},
+    where: {UserId},
     include: [Model.Task]
+  };
+  if(!includeTasks){
+  	delete options.include;
   }
   return Model.Todo.findAll(options);
-}
+};
 
-module.exports = {getTodosForUser}
+const addTodo = (UserId, CategoryId, todoName) => {
+
+  return Model.Todo.create({UserId, CategoryId, name:todoName});
+};
+
+const markAsComplete = (id) => {
+
+  return Model.Todo.update({isCompleted: true}, {where:{id}});
+};
+
+const deleteTodo = (id) => {
+
+  return Model.Todo.destroy({where:{id}})
+};
+
+const getAllTodoWithIncompleteTasksOnly = (UserId) => {
+
+  return Model.Todo.findAll({
+  	where: {UserId},
+  	include:[{model: Model.Task, where:{isCompleted: false}, attributes: {exclude:['isCompleted']}}],
+  	//To just include Task name --> include:[{model: Model.Task, where:{isCompleted: false}}, attributes:['name']]
+  	//To exclude isComplete --> include:[{model: Model.Task, where:{isCompleted: false}, attributes: {exclude:['isCompleted']}}]
+  	raw:true
+  });
+};
+
+
+module.exports = {getTodosForUser, addTodo,markAsComplete, deleteTodo,getAllTodoWithIncompleteTasksOnly};
 
